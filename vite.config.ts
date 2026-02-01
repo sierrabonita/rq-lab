c2f26b4558d0a06b44af1e515dd62cca9c621eb0
sierrabonita <workbj1212@gmail.com>
2026-02-01 12:38:22 +0900

    chore: enhance vite.config.ts to load environment variables and configure server port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+      strictPort: true, // 3000が空いてなければエラーで止める
+    },
+  };
 });
